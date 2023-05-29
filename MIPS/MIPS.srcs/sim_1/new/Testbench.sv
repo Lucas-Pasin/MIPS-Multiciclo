a922cc663d4889c25ecb298695165284a985c1ce
Lucas-Pasin <lucas-pasin@uergs.edu.br>
2023-05-29 20:41:29 -0300

    testbench

--- MIPS/MIPS.srcs/sim_1/new/Testbench.sv
+++ MIPS/MIPS.srcs/sim_1/new/Testbench.sv
@@ -1,15 +1,23 @@
 `timescale 1ns / 1ps
-
 module Testbench(
-   output logic clock,
-   output logic reset
-);
+    output logic clock,
+    output logic reset,
+    input logic halt
+    );
 
+initial begin
+    reset = 0;
+    #6;
+    reset = 1;
+    #12;
+    reset = 0;
+end
 always begin
-    clock <= 0;
-    #10n;
-    clock <= 1;
+    clock = 0;
+    #5;
+    clock = 1;
+    #5;
 end
 
-endmodule : Testbench
 
+endmodule
